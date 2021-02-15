688ab3e90604b3576c28bae52515b1644c9d0829
mksiq <mcksiq@gmail.com>
2021-02-15 12:14:51 -0500

    replace .then for async await

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 /**********************************************************************************************
  *   WEB422 – Assignment 1
- *  
+ *
  *   I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  *   No part of this assignment has been copied manually or electronically from any other source
  *   (including web sites) or distributed to other students.
- *  
+ *
  *   Name: Maickel Siqueira Student ID: 129337192 Date: 2020-01-17
  *   Heroku Link: https://web422-assignment1-msiqueira.herokuapp.com/api/restaurants/
- *  
+ *
  ***********************************************************************************************/
 
 /** I am using import modules instead of requires */
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import RestaurantDB from "./modules/restaurantDB.js";
 
 const app = express();
@@ -25,56 +25,63 @@ const db = new RestaurantDB(process.env.MONGO_DB);
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-db.initialize()
-  .then(() => {
+(async () => {
+  try {
+    await db.initialize();
     app.listen(HTTP_PORT, () => {
       console.log(`Serving on ${HTTP_PORT}`);
       if (HTTP_PORT === 8080) {
-        console.log('Click here to open browser: http://localhost:8080');
+        console.log("Click here to open browser: http://localhost:8080");
       }
     });
-  }).catch((err) => console.error(err));
+  } catch (err) {
+    console.error(err);
+  }
+})();
 
 /**
    GET to test the API
  */
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'API Listening.' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API Listening." });
 });
 
 /** Search restaurant by id. Example:
   GET http://localhost:8080/api/restaurants/5eb3d668b31de5d588f4292e
 */
-app.get('/api/restaurants/:id', (req, res) => {
+app.get("/api/restaurants/:id", async (req, res) => {
   const { id } = req.params;
-  db.getRestaurantById(id).then(restaurant => {
+  try {
+    const restaurant = await db.getRestaurantById(id);
     if (!restaurant) {
-      res.status(407).json({ message: `Unable to find restaurant with id as : ${id}` });
+      res
+        .status(407)
+        .json({ message: `Unable to find restaurant with id as : ${id}` });
     } else {
-      res.status(200).json({ restaurant });
+      res.status(200).json(restaurant);
     }
-  }).catch(err => {
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Something went wrong in the database.' });
-  });
+    res.status(500).json({ message: "Something went wrong in the database." });
+  }
 });
 
 /** Search restaurants by page, num of pages and borough. Example: 
   GET http://localhost:8080//api/restaurants?page=1&perPage=5&borough=Bronx
 */
-app.get('/api/restaurants/', (req, res) => {
+app.get("/api/restaurants/", async (req, res) => {
   const { page, perPage, borough } = req.query;
-
-  db.getAllRestaurants(page, perPage, borough).then(restaurants => {
+  try {
+    const restaurants = await db.getAllRestaurants(page, perPage, borough);
     if (!restaurants || !Array.isArray(restaurants) || !restaurants.length) {
-      res.status(407).json({ message: 'Query didn\'t find any restaurant.' });
+      res.status(407).json({ message: "Query didn't find any restaurant." });
     } else {
-      res.status(200).json({ restaurants });
+      res.status(200).json(restaurants);
     }
-  }).catch(err => {
+  } catch (err) {
     console.error(err);
     res.status(400).json({ message: err.message });
-  });
+  }
 });
 
 /** Inserts a new restaurant to the database
@@ -88,21 +95,22 @@ app.get('/api/restaurants/', (req, res) => {
     "name":"Starbucks","restaurant_id":"01"}
  * Id created with example : 60046df94912e61f5b484cbb 
  */
-app.post('/api/restaurants/', (req, res) => {
+app.post("/api/restaurants/", async (req, res) => {
   const { data } = req.body;
   if (data) {
-    db.addNewRestaurant(data).then(response => {
+    try {
+      const response = await db.addNewRestaurant(data);
       if (!response) {
-        res.status(500).json({ message: 'Something went wrong' });
+        res.status(500).json({ message: "Something went wrong" });
       } else {
         res.status(200).json({ message: response });
       }
-    }).catch(err => {
+    } catch (err) {
       console.error(err);
       res.status(500).json({ message: err.message });
-    });
+    }
   } else {
-    res.status(400).json({ message: 'No restaurant information given.' });
+    res.status(400).json({ message: "No restaurant information given." });
   }
 });
 
@@ -122,54 +130,62 @@ app.post('/api/restaurants/', (req, res) => {
     }
   }
 */
-app.put('/api/restaurants/:id', (req, res) => {
+app.put("/api/restaurants/:id", async (req, res) => {
   const { id } = req.params;
   const { data } = req.body;
   if (data && id) {
-    // Makes sure that the restaurant else it will give message that it was successful
-    // even if the restaurant does not exist.
-    db.getRestaurantById(id).then(restaurant => {
+    try {
+      // Makes sure that the restaurant exists
+      const restaurant = await db.getRestaurantById(id);
       if (!restaurant) {
-        res.status(407).json({ message: `Unable to find restaurant with id as : ${id}` });
+        res
+          .status(407)
+          .json({ message: `Unable to find restaurant with id as : ${id}` });
       } else {
-        // This is where it actually updates
-        db.updateRestaurantById(data, id).then(response => {
+        try {
+          // This is where it actually updates
+          const response = await db.updateRestaurantById(data, id);
           if (!response) {
-            res.status(500).json({ message: 'Something went wrong' });
+            res.status(500).json({ message: "Something went wrong" });
           } else {
             res.status(200).json({ message: response });
           }
-        }).catch(err => {
+        } catch (err) {
           console.error(err);
           res.status(500).json({ message: err.message });
-        });
+        }
       }
-    }).catch(err => {
+    } catch (err) {
       console.error(err);
-      res.status(500).json({ message: 'Something went wrong in the database.' });
-    });
+      res
+        .status(500)
+        .json({ message: "Something went wrong in the database." });
+    }
   } else {
-    res.status(400).json({ message: 'Not enough restaurant information given.' });
+    res
+      .status(400)
+      .json({ message: "Not enough restaurant information given." });
   }
 });
 
 /** Delete one restaurant by id. Example:
   GET http://localhost:8080/api/restaurants/60046df94912e61f5b484cbb 
 */
-app.delete('/api/restaurants/:id', (req, res) => {
+app.delete("/api/restaurants/:id", async (req, res) => {
   const { id } = req.params;
   if (id) {
-    db.deleteRestaurantById(id).then(response => {
+    try {
+      const response = await db.deleteRestaurantById(id);
       if (!response) {
-        res.status(500).json({ message: 'Something went wrong' });
+        res.status(500).json({ message: "Something went wrong" });
       } else {
         res.status(204).json({ message: response });
       }
-    }).catch(err => {
+    } catch (err) {
       console.error(err);
       res.status(500).json({ message: err.message });
-    });
+    }
   } else {
-    res.status(400).json({ message: 'Id not provided.' });
+    res.status(400).json({ message: "Id not provided." });
   }
 });
